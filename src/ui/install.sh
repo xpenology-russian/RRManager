34df4d44b0054b3c6bac88235227b0834313fc3f
Yurii Nazarenko <yurii.nazarenko@tine.no>
2024-06-07 15:32:55 +0200

    #31, Fix priviledges issue during install the package

--- src/ui/install.sh
+++ src/ui/install.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+# clean up OOTB the privilege file
+mv /var/packages/rr-manager/conf/privilege /tmp
+# use the custom privilege file
+mv /var/packages/rr-manager/conf/privilege_ /var/packages/rr-manager/conf/privilege
+# apply root privilege to the package
 sed -i ''s/package/root/g'' /var/packages/rr-manager/conf/privilege
 synopkg restart rr-manager
 cat /var/packages/rr-manager/target/ui/tasks.sql | sqlite3 /usr/syno/etc/esynoscheduler/esynoscheduler.db 
